6bec98492093dffc438f2370a6b2ac2027080b0d
amri97 <39306282+amri97@users.noreply.github.com>
2019-01-06 21:21:39 +0700

    Initial commit

---
+++ app/src/androidTest/java/org/fai/judibola/NewActivityTest.java
@@ -0,0 +1,8 @@
+package org.fai.judibola;
+
+/**
+ * Created by Sandi on 11/28/2018.
+ */
+public class NewActivityTest {
+
+}
---
+++ app/src/main/java/org/fai/judibola/ListViewAdapter.java
@@ -0,0 +1,144 @@
+package org.fai.judibola;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class ListViewAdapter extends BaseAdapter{
+
+    //variables
+    Context mContext;
+    LayoutInflater inflater;
+    List<Model> modellist;
+    ArrayList<Model> arrayList;
+
+    //constructor
+    public ListViewAdapter(Context context, List<Model> modellist) {
+        mContext = context;
+        this.modellist = modellist;
+        inflater = LayoutInflater.from(mContext);
+        this.arrayList = new ArrayList<Model>();
+        this.arrayList.addAll(modellist);
+    }
+
+    public class ViewHolder{
+        TextView mTitleTv, mDescTv;
+        ImageView mIconIv;
+    }
+
+    @Override
+    public int getCount() {
+        return modellist.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return modellist.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(final int postition, View view, ViewGroup parent) {
+        ViewHolder holder;
+        if (view==null){
+            holder = new ViewHolder();
+            view = inflater.inflate(R.layout.row, null);
+
+            //locate the views in row.xml
+            holder.mTitleTv = view.findViewById(R.id.mainTitle);
+            holder.mDescTv = view.findViewById(R.id.mainDesc);
+            holder.mIconIv = view.findViewById(R.id.mainIcon);
+
+            view.setTag(holder);
+
+        }
+        else {
+            holder = (ViewHolder)view.getTag();
+        }
+        //set the results into textviews
+        holder.mTitleTv.setText(modellist.get(postition).getTitle());
+        holder.mDescTv.setText(modellist.get(postition).getDesc());
+        //set the result in imageview
+        holder.mIconIv.setImageResource(modellist.get(postition).getIcon());
+
+        //listview item clicks
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //code later
+                if (modellist.get(postition).getTitle().equals("Adidas")){
+                    //start NewActivity with title for actionbar and text for textview
+                    Intent intent = new Intent(mContext, NewActivity.class);
+                    intent.putExtra("actionBarTitle", "Adidas");
+                    intent.putExtra("contentTv","View Adidas.....");
+                    mContext.startActivity(intent);
+                }
+
+               if (modellist.get(postition).getTitle().equals("Bata")){
+                    //start NewActivity with title for actionbar and text for textview
+                    Intent intent = new Intent(mContext, NewActivity.class);
+                    intent.putExtra("actionBarTitle", "Bata");
+                    intent.putExtra("contentTv", "View Bata.....");
+                    mContext.startActivity(intent);
+                }
+
+                if (modellist.get(postition).getTitle().equals("Batik Keris")){
+                    //start NewActivity with title for actionbar and text for textview
+                    Intent intent = new Intent(mContext, NewActivity.class);
+                    intent.putExtra("actionBarTitle", "Batik Keris");
+                    intent.putExtra("contentTv", "View Batik Keris.....");
+                    mContext.startActivity(intent);
+                }
+
+                if (modellist.get(postition).getTitle().equals("Bilabong")){
+                    //start NewActivity with title for actionbar and text for textview
+                    Intent intent = new Intent(mContext, NewActivity.class);
+                    intent.putExtra("actionBarTitle", "Bata");
+                    intent.putExtra("contentTv", "View Bilabong.....");
+                    mContext.startActivity(intent);
+                }
+
+                if (modellist.get(postition).getTitle().equals("BreadTalk")){
+                    //start NewActivity with title for actionbar and text for textview
+                    Intent intent = new Intent(mContext, NewActivity.class);
+                    intent.putExtra("actionBarTitle", "BreadTalk");
+                    intent.putExtra("contentTv", "View Bread Talk.....");
+                    mContext.startActivity(intent);
+                }
+
+            }
+        });
+
+        return view;
+    }
+    //filter
+    public void filter(String charText){
+        charText = charText.toLowerCase(Locale.getDefault());
+        modellist.clear();
+        if (charText.length()==0){
+            modellist.addAll(arrayList);
+        }
+        else {
+            for (Model model : arrayList){
+                if (model.getTitle().toLowerCase(Locale.getDefault())
+                        .contains(charText)){
+                    modellist.add(model);
+                }
+            }
+        }
+        notifyDataSetChanged();
+    }
+
+}
